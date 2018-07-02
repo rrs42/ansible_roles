bb62dd65cfec4026849b7d82679b9305521d91cc
Russell Steffen <rsteffen@gmail.com>
2018-07-01 21:56:05 -0700

    start ipa server + client roles

---
+++ freeipa_client/templates/ipaclient_config.sh
@@ -0,0 +1,2 @@
+#!/bin/bash -x
+ipa-client-install --domain=core-mem.net -p admin -N -w 1qazxsw2 -U --automount-location=cluster
---
+++ freeipa_server/templates/ipaserver_config.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -x
+
+serial=$(date +%Y%m%d%H%M)
+
+ipa-server-install -N -U -p 1qazxsw2 -a 1qazxsw2 \
+    -n core-mem.net -r CORE-MEM.NET --ca-subject="CN=Certificate Authority, O=CORE-MEM.NET ${serial}" \
+    --hostname=idmserver.core-mem.net
+
+echo 1qazxsw2 | kinit admin
+
+ipa automountlocation-add cluster
+ipa automountmap-add cluster auto.home
+ipa automountkey-add cluster auto.home --key="*" --info="-fstype=bind :/gpfs/fs1/home/&"
+ipa automountkey-add cluster auto.master --key="/home" --info="auto.home"
