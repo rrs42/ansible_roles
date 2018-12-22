e318d758849d0214ec315818b1ee6729e2261d28
Russell Steffen <rsteffen@gmail.com>
2018-12-21 21:34:18 -0800

    start ipa uids at 10000

--- freeipa_server/templates/ipaserver_config.sh
+++ freeipa_server/templates/ipaserver_config.sh
@@ -3,7 +3,9 @@
 serial=$(date +%Y%m%d%H%M)
 
 ipa-server-install -N -U -p 1qazxsw2 -a 1qazxsw2 \
-    -n core-mem.net -r CORE-MEM.NET --ca-subject="CN=Certificate Authority, O=CORE-MEM.NET ${serial}" \
+    -n core-mem.net -r CORE-MEM.NET \
+    --ca-subject="CN=Certificate Authority, O=CORE-MEM.NET ${serial}" \
+    --idstart=10000 \
     --hostname=idmserver.core-mem.net
 
 echo 1qazxsw2 | kinit admin
