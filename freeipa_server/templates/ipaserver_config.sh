8b6ba6d4f04693045afc2077e5518b8272964c9a
Russell Steffen <rsteffen@gmail.com>
2019-02-19 20:39:20 -0800

    add service accounts

--- freeipa_server/templates/ipaserver_config.sh
+++ freeipa_server/templates/ipaserver_config.sh
@@ -10,6 +10,32 @@ ipa-server-install -N -U -p 1qazxsw2 -a 1qazxsw2 \
 
 echo 1qazxsw2 | kinit admin
 
+ipa config-mod --defaultshell=/bin/bash
+
+ipa user-add kafka \
+    --first="Kafka" \
+    --last="User" \
+    --initials="" \
+    --gecos="Kafka service account" \
+    --displayname="Kafka service account" \
+    --homedir=/opt/kafka \
+    --shell=/bin/bash \
+    --random \
+    --uid=1500 \
+    --gidnumber=1500
+
+ipa user-add nifi \
+    --first="NiFi" \
+    --last="User" \
+    --initials="" \
+    --gecos="NiFi service accounr" \
+    --displayname="NiFi service account" \
+    --homedir=/opt/nifi \
+    --shell=/bin/bash \
+    --random \
+    --uid=1501 \
+    --gidnumber=1501
+
 ipa automountlocation-add cluster
 ipa automountmap-add cluster auto.home
 ipa automountkey-add cluster auto.home --key="*" --info="-fstype=nfs4 10.80.0.232:/srv/exports/home/&"
